e0f230c71142b374d4917c8b4ddb19650696582d
juanferracioli2024@campus-eni.fr <ferraciolijuan@gmail.com>
2025-01-13 16:42:35 +0000

    image do cliente

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_07_150110) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_13_162151) do
   create_table "clients", force: :cascade do |t|
     t.string "name"
     t.string "client_type"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_07_150110) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "prices", force: :cascade do |t|
