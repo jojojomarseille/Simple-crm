741efb181c84e548d5b0ea3e88e4db87c6217e0e
Joachim Bourquardez <j.bourquardez@laposte.net>
2025-01-26 21:19:24 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,27 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_20_162402) do
     t.decimal "longitude"
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["client_id"], name: "index_orders_on_client_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "prices", force: :cascade do |t|
     t.decimal "amount"
     t.string "currency"
@@ -60,5 +81,9 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_20_162402) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "orders", "clients"
+  add_foreign_key "orders", "users"
   add_foreign_key "prices", "products"
 end
