41904577bd38f3ba15460e370d6013e5dc9170e9
juanferracioli2024@campus-eni.fr <ferraciolijuan@gmail.com>
2025-01-27 14:58:03 +0000

    TENTATIVA resolver pesquisa home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Rota para a pesquisa
-  get 'search', to: 'home#index', as: 'search' 
+  get 'search', to: 'home#search', as: 'search' 
 
   resources :products do
     patch 'update_price/:price_id', to: 'products#update_price', as: 'update_price'
