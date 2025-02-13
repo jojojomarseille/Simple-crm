edf0e4356abf861abd8168f24e2ec982fe99b17b
juanferracioli2024@campus-eni.fr <ferraciolijuan@gmail.com>
2025-02-13 15:28:47 +0000

    menu lateral DROPBAR

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_direcrory ../style_form .css
+//= link_direcrory ../edit_form_style .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
