6f528f8f4b68951f6b2a8a20c4de30a43fb1b94a
Joachim Bourquardez <j.bourquardez@laposte.net>
2025-05-11 21:18:22 +0100

    Renommage du projet de super-crm-test à nouveau-nom

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 require_relative "../app/middleware/system_status_middleware"
 
-module SuperCrmTest
+module SimpleCrm
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
